4731bbcf1678d90848030799734155a4f9e20545
Claire Devillers <clairedevillersca@gmail.com>
2024-12-09 22:19:44 +0100

    add create functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@
 import { useState, useEffect } from 'react';
 import * as petService from './services/petService'
 import PetList from './components/PetList';
+import PetDetail from './components/PetDetail'
+import PetForm from './components/PetForm';
 
 
 const App = () => {
   const [petList, setPetList] = useState([]);
+  const [selected, setSelected] = useState(null); 
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   useEffect(() => {
     const fetchPets = async () => {
@@ -24,7 +28,49 @@ const App = () => {
     fetchPets();
   }, []);
 
-  return <PetList petList={petList} />;
+  // in the petList, when someone clicks on, eg., Jeff2
+  // that gets added into "selected" state in the parent
+  const updateSelected = (pet) => {
+    setSelected(pet)
+  }
+
+  const handleFormView = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddPet = async (formData) => {
+    try {
+      // Call petService.create, assign return value to newPet
+      const newPet = await petService.create(formData);
+
+      if (newPet.error) {
+        throw new Error(newPet.error);
+      }
+      // Add the pet object and the current petList to a new array, and
+      // set that array as the new petList
+      setPetList([newPet, ...petList]);
+      setIsFormOpen(false);
+    } catch (error) {
+        // Log the error to the console
+        console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <PetList
+        petList={petList}
+        updateSelected={updateSelected}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+        <PetForm handleAddPet={handleAddPet} />
+      ) : (
+        <PetDetail selected={selected} />
+      )}
+    </>
+  );
 };
 
 export default App;
--- src/services/petService.js
+++ src/services/petService.js
@@ -9,4 +9,24 @@ const index = async () => {
     }
 };
 
-export { index };
+const create = async (formData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      // We specify that this is a 'POST' request
+      method: 'POST',
+      // We're sending JSON data, so we attach a Content-Type header
+      // and specify that the data being sent is type 'application/json'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // The formData, converted to JSON, is sent as the body
+      // This will be received on the backend as req.body
+      body: JSON.stringify(formData),
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { index, create };
